b91a957d6c0104023a94f0e890429b654f642eb7
Injener969 <injener969@example.com>
2026-03-11 23:16:29 +0300

    fix: explicit featured web3modal wallets

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,13 @@ createWeb3Modal({
   projectId,
   enableAnalytics: false,
   allWallets: 'SHOW', // Show all available wallets
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+    '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369', // Rainbow
+    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase Wallet
+    'v1', // Generic WalletConnect
+  ],
   themeVariables: {
     '--w3m-accent': '#E84142',
     '--w3m-border-radius-master': '10px',
