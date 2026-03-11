2a0e4799dd45cc0a9f1eed535795258c7a8f9370
Injener969 <injener969@example.com>
2026-03-11 23:20:42 +0300

    feat: explicitly feature Bitget Wallet in connection modal

--- src/App.tsx
+++ src/App.tsx
@@ -53,11 +53,13 @@ createWeb3Modal({
   enableAnalytics: false,
   allWallets: 'SHOW', // Show all available wallets
   featuredWalletIds: [
+    '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662', // Bitget Wallet
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
     '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
     '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369', // Rainbow
     'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase Wallet
     'v1', // Generic WalletConnect
+
   ],
   themeVariables: {
     '--w3m-accent': '#E84142',
